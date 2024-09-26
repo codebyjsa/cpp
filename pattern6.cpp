b1f3539e4ad5ad3454a1770b0132159795a6ebd6
codebyjsa <jsaishere@gmail.com>
2024-09-26 23:23:05 +0530

    just an edit

--- pattern6.cpp
+++ pattern6.cpp
@@ -5,12 +5,10 @@ int main(){
 
     int n;
     cin>>n;
-    int num=1;
 
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
-            cout<<num;
-            num++;
+            cout<<i+1;
         }
         cout<<endl;
     }

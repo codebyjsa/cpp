baf2d45216a8be8e126ec03b97ab8f93784360c5
codebyjsa <jsaishere@gmail.com>
2024-09-27 07:37:47 +0530

    new commit

--- pattern10.cpp
+++ pattern10.cpp
@@ -6,7 +6,7 @@ int main(){
     int n;
     cin>>n;
 
-    for(int i=0; i<(n-1); i++){
+    for(int i=0; i<n; i++){
         for(int j=(i+1); j>0; j--){
             cout<<j;
         }
--- prime.cpp
+++ prime.cpp
@@ -1,39 +1,37 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-bool isprime(int n){
-    if (n<=1)
-    {
+bool isPrime(int n) {
+  
+    // If the number is less than or equal to 1, it is not
+    // prime
+    if (n <= 1) {
         return false;
     }
-    else
-    {
-        for(int i=2; i<n; i++){
-            if (i%n==0)
-            {
-                return false;
-            }
-            
+
+    // Check for divisors from 2 to n-1
+    for (int i = 2; i < n; i++) {
+      
+        // If n is divisible by any number in this range, it
+        // is not prime
+        if (n % i == 0) {
+            return false;
         }
     }
-    
-    return true; 
-    
+
+    // If no divisors are found, it is prime
+    return true;
 }
 
-int main(){
-    int n;
-    cout<<"Write a number:";
-    cin>>n;
+int main() {
+    int n = 10;
 
-    if (isprime(n))
-    {
-        cout<<"It is prime number!"<<endl;
-    }
-    else
-    {
-        cout<<"Not a prime number"<<endl;
-    }
-    
-    
+    if (isPrime(n)){
+        cout<< "Prime" << endl;
+        }
+    else{
+        cout << "Not Prime" << endl;
+        }
+
+    return 0;
 }
